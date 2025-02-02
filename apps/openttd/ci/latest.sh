92c368446738fb7272623814640a6459e08c10e0
Jared <mail@sillock.com>
2025-02-02 14:59:59 +0000

    feat: support openttd pre-releases

--- apps/openttd/ci/latest.sh
+++ apps/openttd/ci/latest.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-version="$(curl -sX GET "https://api.github.com/repos/openttd/openttd/releases/latest" | jq --raw-output '.tag_name' 2>/dev/null)"
+version="$(curl -sX GET https://api.github.com/repos/openttd/openttd/releases | jq -r 'first | .tag_name' 2>/dev/null)"
 version="${version#*v}"
 version="${version#*release-}"
 printf "%s" "${version}"
